52145ef998422bd84d3e54dfe0479d562dbb8f7a
mansuiki <mansuiki@gmail.com>
2019-06-23 19:45:54 +0900

    작업환경 변경 Xcode -> Clion

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 //
 
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
