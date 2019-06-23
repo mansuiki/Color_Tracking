e0011b87301fd092219813e9c3047e439f7f3ebb
mansuiki <mansuiki@gmail.com>
2019-06-23 15:33:41 +0900

    Add Readme & gitignore & data

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Color_Tracking
+//
+//  Created by GyeongTaek Kim on 23/06/2019.
+//  Copyright © 2019 GyeongTaek Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
